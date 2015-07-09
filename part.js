d6745a4ab2f7be561ff225a1b2a22853ab52af9e
VINESH KANNAN <vingkan@gmail.com>
2015-07-09 11:45:53 -0500

    Defining rotate function for part.js

--- part.js
+++ part.js
@@ -1,3 +1,7 @@
+/*
+* Constructor for Part class
+* Represents a single part of a Rubik's Cube (a miniature cube)
+*/
 function Part(temp){
 	this.up = null;
 	this.down = null;
@@ -9,6 +13,10 @@ function Part(temp){
 	this.last = new Part();
 }
 
+/*
+* var face (string): the face to be rotated
+* var clockwise (boolean): rotate face clockwise, counter-clockwise if false
+*/
 Part.prototype.rotate = function(face, clockwise){
 	if(clockwise){
 		switch(face){
@@ -24,7 +32,33 @@ Part.prototype.rotate = function(face, clockwise){
 				this.left = this.last.back;
 				this.front = this.left;
 				break;
+			case 'front':
+				this.up = this.last.left;
+				this.right = this.last.up;
+				this.down = this.last.right;
+				this.left = this.last.down;
+				break;
+			case 'back':
+				this.up = this.last.right;
+				this.right = this.last.down;
+				this.down = this.last.left;
+				this.left = this.last.up;
+				break;
+			case 'left':
+				this.up = this.last.back;
+				this.back = this.last.down;
+				this.down = this.last.front;
+				this.front = this.last.up;
+				break;
+			case 'right':
+				this.up = this.last.front;
+				this.front = this.last.down;
+				this.down = this.last.back;
+				this.back = this.last.up;
+				break;
+			default:
+				alert("Invalid Face.");
+				break;
 		}
 	}
-
 }
